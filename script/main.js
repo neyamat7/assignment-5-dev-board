a765417772389ab7286bd27164fd23f0c7e82b53
Neyamat Ullah <neyamat7.ullah@gmail.com>
2025-03-02 10:46:26 +0600

    finally final touch

--- script/main.js
+++ script/main.js
@@ -9,19 +9,16 @@ for (const btn of completeBtn) {
     event.target.setAttribute("disabled", "");
     event.target.classList.remove("cursor-pointer");
     event.target.classList.add("cursor-not-allowed");
-    console.log(event.target);
 
     // dynamic subtract task value functionality
     // get initial task value
     const taskValue = document.getElementById("task-value");
     const convertedTaskValue = parseInt(taskValue.innerText);
     taskValue.innerText = convertedTaskValue - 1;
-    console.log(convertedTaskValue - 1);
 
     // get complete-task to increase
     const completeTaskValue = document.getElementById("complete-task");
     const convertedCompleteTaskValue = parseInt(completeTaskValue.innerText);
-    console.log(convertedCompleteTaskValue);
     completeTaskValue.innerText = convertedCompleteTaskValue + 1;
 
     // successful alert after clicking complete button
